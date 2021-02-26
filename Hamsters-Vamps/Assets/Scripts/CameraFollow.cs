9398d629f326970fbc5e7530e0173559c39e733c
HlexNC <rudaevalex@gmail.com>
2021-02-26 14:07:35 +0300

    Add Player Camera

---
+++ Hamsters-Vamps/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    [SerializeField] Vector3 offset = new Vector3(0, 16, -15);
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = player.position + offset;
+    }
+}
---
+++ Hamsters-Vamps/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _playerRb;
+    [SerializeField] float _playerInputVer;
+    [SerializeField] float _playerInputHor;
+    [SerializeField] int playerSpeed=10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _playerInputVer = Input.GetAxis("Vertical");
+        _playerInputHor = Input.GetAxis("Horizontal");
+        _playerRb.AddForce(Vector3.forward * (playerSpeed * _playerInputVer));
+        _playerRb.AddForce(Vector3.right * (playerSpeed * _playerInputHor));
+    }
+    /*
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Water")) {
+            gameObject.transform.position = new Vector3(0, 1, 0); }
+    }
+    */
+}
